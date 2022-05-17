5a43101c58bbb88ec3e4da807bd09a80566aabb8
Sang Hyeok Hwang <cdnwellhk@gmail.com>
2022-05-17 21:09:19 +0900

    part1. programmers test

---
+++ test23/src/programmers/NumberStringEn.java
@@ -0,0 +1,26 @@
+package programmers;
+
+public class NumberStringEn {
+	/*
+	 * level 1 : 숫자 문자열과 영단어
+	 */
+	public static void main(String[] args) {
+		String str = "one4seveneight";
+		
+		System.out.println(solution(str));
+		
+		
+	}
+	
+	public static int solution(String s) {
+        int answer = 0;
+        char [] strBox = s.toCharArray();
+        
+        for(int i =0;i<strBox.length;i++) {
+        	
+        }
+        
+        return answer;
+    }
+	
+}
