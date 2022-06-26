d7454d9c268f36d97940cb57bf5724aae573d972
Sang Hyeok Hwang <cdnwellhk@gmail.com>
2022-06-26 10:55:28 +0900

    commit 6.26

---
+++ w_tmp/src/stream/StreamTest.java
@@ -0,0 +1,18 @@
+package stream;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+import org.w3c.dom.Node;
+
+public class StreamTest {
+
+	public static void main(String[] args) {
+		
+		 Node[] tab = new Node[5];s
+		 ArrayList<String> list = new ArrayList<>();
+ 		 Iterator <String> it = list.iterator();
+	}
+
+}
