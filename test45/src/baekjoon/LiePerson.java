8aee8699f6f0e3ca149d29de0f58b75f9f5f2c71
Sang Hyeok Hwang <cdnwellhk@gmail.com>
2022-07-30 00:10:08 +0900

    test practice 7.30

--- test45/src/baekjoon/LiePerson.java
+++ test45/src/baekjoon/LiePerson.java
@@ -1,7 +1,9 @@
 package baekjoon;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class LiePerson {
@@ -38,30 +40,69 @@ public class LiePerson {
 		}//파티에 오는 사람과 오는 사람의 번호를 LinkedList에 넣는다.
 		//LinkedList 사용 이유는 제거하기 쉽게 하기 위해서 결과 값으로 LinkedList 사이즈를 받음
 		
+		Queue<Integer> queue = new LinkedList<>();
+		boolean [] checkParty = new boolean[M+1]; // 번호를 맞춰주기 위해 +1
+		Arrays.fill(checkParty, Boolean.FALSE);
+		boolean [] checkPerson = new boolean[N+1];
+		Arrays.fill(checkPerson, Boolean.FALSE);
+		checkParty[0] = true;
+		
+//		int countingNum = 0;
+//		for(boolean f : checkPerson) {
+//			System.out.println(countingNum + " " + f);
+//			countingNum++;
+//		}
+		//boolean 배열 값 확인을 위한 부분
+		
+		HashSet<Integer> setPeople = new HashSet<>();
+		
+		for(int i : set) {
+			queue.add(i);
+			setPeople.add(i);
+		}//거짓말을 알고 있는 사람 번호 큐에 입력
+		
 		//---------------------입력 완료---------------------//
 		
 		// 1. 영향을 받은 사람 검색하기
 		
-		for(int num : set) {
-			for(String s : list) {
+		
+		
+		while(!queue.isEmpty()) {
+			int knowPerson = queue.poll();
+			for(int j =0;j<list.size();j++) {
+				String s = list.get(j);
 				int count = 0;
-				String[] s_split = s.split(" ");
-				int s_num = Integer.parseInt(s_split[0]);
-				for(int i = 1; i<=s_num ; i++) {
-					int s_tmp = Integer.parseInt(s_split[i]);
-					if(num == s_tmp) {
+				String []sArr = s.split(" ");
+				int numPerson = Integer.parseInt(sArr[0]);
+				for(int i = 1; i <= numPerson ; i++) {
+					if(Integer.parseInt(sArr[i])==knowPerson) {
 						count++;
 					}
 				}
 				if(count != 0) {
-					for(int i = 1;i<=s_num ; i++) {
-						set.add(Integer.parseInt(s_split[i]));
+					for(int i =1 ; i<= numPerson ; i++) {
+						int tmpNum = Integer.parseInt(sArr[i]);
+						checkParty[tmpNum] = true;
+						if(checkPerson[tmpNum]==true) continue;
+						checkPerson[tmpNum] = true;
+						queue.add(tmpNum);
 					}
-				}
+				}//큐의 값이 문자열 안의 숫자와 일치하면 if문을 실행시키고 처음부터 끝까지의 수 모두를 큐에 넣는다.
 			}
-		}// for each문 끝
+		}//해시에 값 다 넣음
+
+		//---------------------입력 완료---------------------//
 		
-		System.out.println(set.toString());
+		// 2. checkParty 배열로 파티 체크하기
+		int result = 0;
+		for(int i = 0; i<checkParty.length;i++) {
+			if(!checkParty[i]) {
+				result++;
+				System.out.println(i);
+			}
+		}
+		
+		System.out.println(result);
 		
 	}
 
