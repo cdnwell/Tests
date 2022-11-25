c5350a2a88fbd3242010399e093a94fec96c03f7
cdnwell <cdnwellhk@gmail.com>
2022-11-25 22:19:54 +0900

    test commit

---
+++ test59/src/bj2839/Main.java
@@ -0,0 +1,30 @@
+package bj2839;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int fiveShare = N / 5;
+        int remainder = 0;
+        int i = fiveShare, j = 0;
+        int result = 0;
+
+        for(i=fiveShare;i>=0;i--){
+            remainder = N - (5 * i);
+            remainder = remainder / 3;
+            for(j=0;j<=remainder;j++){
+                if((5 * i) + (3 * j) == N) break;
+            }
+            if((5 * i) + (3 * j) == N) break;
+            if(i==0 && j==remainder+1) result = -1;
+        }
+
+        if(result != -1) result = i+j;
+
+        System.out.println(result);
+
+    }
+}
