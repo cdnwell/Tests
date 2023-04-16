2d51da6b05f136e4560712b08bcf14f4163d0702
cdnwell <cdnwellhk@gmail.com>
2023-04-16 10:54:21 +0900

    solving : pg lv.2 test

---
+++ test85/src/pg154538/ConvertNumbers.java
@@ -0,0 +1,18 @@
+package pg154538;
+
+public class ConvertNumbers {
+
+    public static void main(String[] args) {
+        int x = 10, y = 40, n = 5;
+
+    }
+
+    public static int solution(int x, int y, int n) {
+        int answer = 0;
+
+
+
+        return answer;
+    }
+
+}
