f8e46c8d27bfdd4666bc622bb1b322d3ca4a7ffe
cdnwell <cdnwellhk@gmail.com>
2023-06-02 22:55:47 +0900

    commit : daily commit

--- test88/src/pg150369/DeepCopy.java
+++ test88/src/pg150369/DeepCopy.java
@@ -1,6 +1,14 @@
 package pg150369;
 
 public class DeepCopy {
+
+    public class Inner {
+        private int a;
+        public int s;
+        public void print() {
+            System.out.println("Inner Class");
+        }
+    }
     public static void main(String[] args) {
         System.out.println("깊은 복사 예시");
     }
