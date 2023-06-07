d4a4f36167b560d7099fa3b58f990f0a46d80633
cdnwell <cdnwellhk@gmail.com>
2023-06-07 21:52:17 +0900

    study : sftp connection class recommended

---
+++ test88/src/pg150369/SFTPClass.java
@@ -0,0 +1,17 @@
+package pg150369;
+
+public class SFTPClass {
+    public static void main(String[] args) {
+        // SFTP 연결을 위한 클래스 추천
+
+        // 1. JSch
+        // 2. Apache Commons VFS
+        // 3. SSHJ
+        // 4. J2SSH Maverick
+        // 5. Ganymed SSH-2 for Java
+        // 6. SSHTools
+        // 7. SSHFactory
+        // 8. SSH API
+
+    }
+}
