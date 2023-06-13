a1b2a5fa4d073b9a9fc9e7ea1b1759d7c4f56fdf
cdnwell <cdnwellhk@gmail.com>
2023-06-13 22:12:31 +0900

    study : union find에 대한 간단한 복습

---
+++ test88/src/pg150369/UnionFindText.java
@@ -0,0 +1,12 @@
+package pg150369;
+
+public class UnionFindText {
+
+    public static void main(String[] args) {
+        // union find 문제
+        // union find 문제는 서로소 집합을 표현하는 알고리즘이다.
+        // union find 문제는 서로소 집합을 표현하는 알고리즘이므로 서로소 집합을 표현하는 알고리즘을 사용해야 한다.
+        // union find 문제는 서로소 집합을 표현하는 알고리즘이므로 union find 알고리즘을 사용해야 한다.
+    }
+
+}
