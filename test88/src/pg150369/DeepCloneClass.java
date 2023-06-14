5663429dd36279d18f283c704800f8a1c5fed4e5
cdnwell <cdnwellhk@gmail.com>
2023-06-14 22:10:55 +0900

    study : deep clone problem

---
+++ test88/src/pg150369/DeepCloneClass.java
@@ -0,0 +1,10 @@
+package pg150369;
+
+public class DeepCloneClass {
+    public static void main(String [] args) {
+        // Deep Clone 문제
+        // Deep Clone 문제는 깊은 복사를 표현하는 알고리즘이다.
+        // Deep Clone 문제는 깊은 복사를 표현하는 알고리즘이므로 깊은 복사를 표현하는 알고리즘을 사용해야 한다.
+
+    }
+}
