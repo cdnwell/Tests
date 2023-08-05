b00bd96b477674cea8d2dcd911cfe02af0cdcede
cdnwell <cdnwellhk@gmail.com>
2023-08-05 11:31:53 +0900

    commit ; daily coding test 08-05

---
+++ test95/src/Main.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int pls = 0;
+        while(true) {
+            int count = 0;
+            int tmp = N + pls;
+
+            while(tmp > 0) {
+                if(tmp % 2 != 0) count++;
+                tmp /= 2;
+            }
+
+            if (count <= K) break;
+
+            pls++;
+        }
+
+        System.out.println(pls);
+    }
+
+}
